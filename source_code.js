22b13deb1980b527cd93835b3a3ac855397568fa
Davis <schooldavissmith@gmail.com>
2021-06-06 20:23:36 -0600

    create the message and log the message to console code

--- source_code.js
+++ source_code.js
@@ -1,17 +1,21 @@
 //A list of various word types to be used in message generation
-let adverbList = [];
-let verbList = [];
-let adjectiveList = [];
+const adverbList = ["Never"];
+const verbList = ["code"];
+const adjectiveList = ["sleeping"];
 
 //function that will generate a random message
 const generateMessage = function(){
 
     //Begin by randomly choosing one word from word lists
-    let adverb = adverbList[Math.floor(Math.random() * adverbList.length)];
-    let verb = verbList[Math.floor(Math.random() * verbList.length)];
-    let adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
+    const adverb = adverbList[Math.floor(Math.random() * adverbList.length)];
+    const verb = verbList[Math.floor(Math.random() * verbList.length)];
+    const adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
 
-    
+    //generate the message by using string interpolation
+    const message = `${adverb} ${verb} while ${adjective}!`;
+
+    //log the message to the console
+    console.log(message);
 };
 
 generateMessage();
