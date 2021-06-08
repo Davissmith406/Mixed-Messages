4c76bd090e424038a039ad546e263a113db41d18
Davis <schooldavissmith@gmail.com>
2021-06-08 12:48:47 -0600

    add more words/phrases to word lists

--- source_code.js
+++ source_code.js
@@ -1,7 +1,7 @@
 //A list of various word types to be used in message generation
-const adverbList = ["Never"];
-const verbList = ["code"];
-const adjectiveList = ["sleeping"];
+const adverbList = ["Never", "Always", "Sometimes", "Occasionally", "Seldom"];
+const verbList = ["code", "program", "use bash", "write JS", "write python", "learn to code"];
+const adjectiveList = ["while sleeping", "on a bike", "while running", "while eating a hamburger", "while drunk", "with the homies"];
 
 //function that will generate a random message
 const generateMessage = function(){
